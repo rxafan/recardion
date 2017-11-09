ffa11b456d9ea14618e29badffb237efa7313e55
Roman Nikiforov <rxafan@gmail.com>
2017-11-09 10:23:57 +0300

    Update recardion.js

--- recardion.js
+++ recardion.js
@@ -1,12 +1,27 @@
 ;(function () {
-    var recardion = document.querySelectorAll('.recardion h4');
+
+    var recardion = document.querySelectorAll('.recardion');
 
     recardion.forEach(function (t, i) {
-        recardion[i].onclick =  _onClick;
+
+        recardion[i].onclick = function (event) {
+
+            var target = event.target;
+            var h = target.closest('h4');
+            if (!h) return;
+            if (!recardion[i].contains(h)) return;
+
+            _onClick(h);
+
+            // Останавливаем всплытие события
+            event.stopPropagation();
+        };
+
     });
 
-    function _onClick() {
-        this.parentElement.classList.toggle("active");
+
+    function _onClick(h) {
+        h.parentElement.classList.toggle("active");
     }
 
 })();
